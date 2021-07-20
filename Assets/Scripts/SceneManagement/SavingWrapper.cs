e0bd3d85faaa4653b93b839b9ab1e52c7c052fed
Dreaw Riddleton <dreawriddleton@hotmail.com>
2021-07-20 18:40:24 +0200

    Saving V1

---
+++ Assets/Scripts/SceneManagement/SavingWrapper.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using UnityEngine;
+using RPG.Saving;
+
+namespace RPG.SceneManagement
+{
+    public class SavingWrapper : MonoBehaviour
+    {
+        const string defaultQuickSave = "QuickSave";
+        const string defaultSave = "Save";
+
+        private IEnumerator Start()
+        {
+            yield return GetComponent<SavingSystem>().LoadLastScene(defaultSave);
+        }
+
+        void Update()
+        {
+            if (Input.GetKeyDown(KeyCode.F5))
+            {
+                GetComponent<SavingSystem>().Save(defaultQuickSave);
+            }
+            if (Input.GetKeyDown(KeyCode.F9))
+            {
+                GetComponent<SavingSystem>().Load(defaultQuickSave);
+            }
+            if (Input.GetKeyDown(KeyCode.S))
+            {
+                Save();
+            }
+            if (Input.GetKeyDown(KeyCode.L))
+            {
+                Load();
+            }
+        }
+
+        public void Save()
+        {
+            GetComponent<SavingSystem>().Save(defaultSave);
+        }
+
+        public void Load()
+        {
+            GetComponent<SavingSystem>().Load(defaultSave);
+        }
+    }
+}
+
