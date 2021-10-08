f0c69f6df6b7c097a0f898dda39e05d60f5369ec
JustinProoo <justincomans.2003@gmail.com>
2021-10-08 15:38:20 +0200

    working gun

--- Assets/Scripts/Core/GunBehavior.cs
+++ Assets/Scripts/Core/GunBehavior.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 using UnityEngine.XR.Interaction.Toolkit;
 
 [RequireComponent(typeof(XRGrabInteractable))]
-public class Gun : MonoBehaviour
+public class GunBehavior : MonoBehaviour
 {
     [SerializeField] Transform m_shootingPoint;
     [SerializeField] GameObject m_bulletPrefab;
@@ -13,11 +13,14 @@ public class Gun : MonoBehaviour
 
     private void Start()
     {
-        bulletSpeed = gunStats.bulletSpeed;
+        if (gunStats)
+            bulletSpeed = gunStats.bulletSpeed;
+        else bulletSpeed = bulletSpeed <= 0 ? 1 : bulletSpeed;
     }
 
     public void Shoot()
     {
+        // use events for bullet effects?
         GameObject _bullet = Instantiate(m_bulletPrefab, m_shootingPoint.position, transform.rotation);
         _bullet.GetComponent<Rigidbody>().AddForce(m_shootingPoint.forward * bulletSpeed);
     }
